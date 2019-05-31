2cf9f3a2d3f1757f086e199858051a5f1445a845
bwpeck <benjaminwade483@yahoo.com>
2019-05-31 13:27:11 -0400

    actions done, now to the app

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import {rootReducer} from './reducers/index';
+import { rootReducer } from './reducers/index';
 
 const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
+  rootReducer,
+  applyMiddleware(thunk)
 );
 
 ReactDOM.render(
